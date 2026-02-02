467ca609c3f6ecf63be5cad814e296b6513e3967
root <root@LAPTOP-UVKCIH47>
2026-02-02 16:50:21 +0700

    Update

--- src/resources/views/livewire/show-data-pinjam.blade.php
+++ src/resources/views/livewire/show-data-pinjam.blade.php
@@ -3,7 +3,7 @@
     <br>
     <br>
     <br>
-    <h1 class="text-2xl font-bold mb-4">Data Pendaftaran Murid Ekskul </h1>
+    <h1 class="text-2xl font-bold mb-4">Data Pendaftaran Mahasiswa Pinjam Barang </h1>
 
     <div class="overflow-x-auto">
         <table class="min-w-full border border-gray-300">
@@ -23,7 +23,15 @@
                      @else
                     @foreach ($pinjams as $data)
                         <tr>
-                            <td class="border px-4 py-2">{{ $data->barang_id}}</td>
+                            <td class="border px-4 py-2">
+                                @switch($data->barang_id)
+                                    @case(1) Laptop Asus @break
+                                    @case(2) Proyektor Epson @break
+                                    @case(3) Speaker JBL @break
+                                    @case(4) Kabel HDMI @break
+                                    @default Barang Tidak Diketahui
+                                @endswitch
+                            </td>
                             <td class="border px-4 py-2">{{ $data->nama_peminjam}}</td>
                             <td class="border px-4 py-2">{{ $data->tanggal_pinjam}}</td>
                             <td class="border px-4 py-2">{{ $data->tanggal_kembali}}</td>
